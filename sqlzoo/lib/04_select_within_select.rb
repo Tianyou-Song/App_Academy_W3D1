4aa69f981d76c580a17d5b378621551bf6e56941
Tianyou_Song <tim.d.song@gmail.com>
2018-09-03 17:34:13 -0700

    Finish lib/ 1-6

--- sqlzoo/lib/04_select_within_select.rb
+++ sqlzoo/lib/04_select_within_select.rb
@@ -8,6 +8,11 @@
 #  population  :integer
 #  gdp         :integer
 
+# Find every country that belongs to a continent where each country's
+# population is less than 25,000,000. Show name, continent and
+# population.
+# Hint: Sometimes rewording the problem can help you see the solution.
+
 require_relative './sqlzoo.rb'
 
 # A note on subqueries: we can refer to values in the outer SELECT within the
@@ -35,6 +40,19 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+    SELECT 
+      name 
+    FROM 
+      countries 
+    WHERE 
+      population > (
+        SELECT 
+          population 
+        FROM 
+          countries 
+        WHERE 
+          name = 'Russia'
+      )
   SQL
 end
 
@@ -42,6 +60,20 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT 
+      name
+    FROM 
+      countries 
+    WHERE 
+      continent = 'Europe' AND
+      gdp / population > (
+        SELECT 
+          gdp / population
+        FROM 
+          countries 
+        WHERE 
+          name = 'United Kingdom'
+      )
   SQL
 end
 
@@ -49,6 +81,20 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT 
+      name, continent
+    FROM 
+      countries 
+    WHERE 
+      continent IN (
+        SELECT 
+          continent 
+        FROM 
+          countries 
+        WHERE 
+          name LIKE '%Belize%' OR
+          name LIKE '%Belgium%'
+      )
   SQL
 end
 
@@ -56,14 +102,93 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+    SELECT 
+      name, population
+    FROM 
+      countries 
+    WHERE 
+      population IN (
+        SELECT 
+          population 
+        FROM 
+          countries 
+        WHERE
+          (name != 'Canada' AND name != 'Poland') AND
+          population BETWEEN (
+            SELECT 
+              population
+            FROM 
+              countries 
+            WHERE
+              name = 'Canada'
+          ) AND (
+            SELECT 
+              population
+            FROM 
+              countries 
+            WHERE
+              name = 'Poland'
+          ) 
+          
+      )
   SQL
 end
 
 def sparse_continents
-  # Find every country that belongs to a continent where each country's
-  # population is less than 25,000,000. Show name, continent and
+  # Find every country that belongs to  
+  
+  #a continent where 
+  
+  #each country's 
+  
+  #population is less than 25,000,000. 
+  
+  #Show name, continent and
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  
+  -- SELECT 
+  --   name, continent, population
+  -- FROM 
+  --   countries 
+  -- WHERE
+  --   continent NOT IN (
+  --     SELECT 
+  --       DISTINCT continent
+  --     FROM 
+  --       countries 
+  --     WHERE 
+  --       population > 25000000
+  --   )
+  
+  
+    SELECT 
+      name, continent, population
+    FROM 
+      countries 
+    WHERE
+      continent IN (
+        SELECT 
+          continent
+        FROM 
+          countries 
+        GROUP BY 
+          continent 
+        HAVING 
+          MAX(population) < 25000000
+      )
+    
+      
+      
+  
+    -- SELECT 
+    --   name
+    -- FROM 
+    --   countries 
+    -- WHERE
+    --   population < 25000000
+     
+      
   SQL
 end
